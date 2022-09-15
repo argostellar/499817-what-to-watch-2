12d1d2a0b4c30fe073b8651d86cc850dd0aff9b4
argostellar <arskud@gmail.com>
2022-09-15 13:56:24 +0700

    m1t2: Добавляет пакет chalk, раскрашивает вывод в консоль

--- src/cli-command/import.command.ts
+++ src/cli-command/import.command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import TSVFileReader from '../common/file-reader/tsv-file-reader.js';
 import { CliCommandInterface } from './cli-command.interface.js';
 
@@ -8,14 +9,14 @@ export default class ImportCommand implements CliCommandInterface {
 
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
+      console.log(chalk.gray(fileReader.toArray()));
     } catch (err) {
 
       if (!(err instanceof Error)) {
         throw err;
       }
 
-      console.log(`Не удалось импортировать данные из файла по причине: «${err.message}»`);
+      console.log(`Не удалось импортировать данные из файла по причине: «${chalk.red(err.message)}»`);
     }
   }
 }
